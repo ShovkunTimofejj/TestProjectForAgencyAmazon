d77fe1b369edf52337b0e09b93802a95480adaf0
shovk <shovkyntimofej@gmail.com>
2024-11-07 17:35:02 +0200

    first commit

---
+++ src/main/java/com/example/testprojectforagencyamazon/TestProjectForAgencyAmazonApplication.java
@@ -0,0 +1,13 @@
+package com.example.testprojectforagencyamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectForAgencyAmazonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectForAgencyAmazonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testprojectforagencyamazon/TestProjectForAgencyAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testprojectforagencyamazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectForAgencyAmazonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
